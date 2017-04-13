3fe3249944b94ea071a5d492af48472f5db70bbf
Andrew Wood <AJWOOD@andrew-woods-imac.local>
2017-04-12 21:48:21 -0400

    Committed Files

---
+++ Support Files/TCPP/dfsurvey.h
@@ -0,0 +1,21 @@
+/* 
+    dfsurvey.stub
+
+    minimum necessary to compile fbfsurvey
+*/
+
+#ifndef _DFSURVEY_H
+#define _DFSURVEY_H
+
+#include <vector.h>
+#include <deque.h>
+
+namespace fsu
+{
+
+  template < class G >
+  class DFSurvey;
+
+} // namespace fsu
+
+#endif
